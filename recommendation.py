e9e68fb0314be9ee495cead69efbb21377d114bf
yousef <mr.ysaj@gmail.com>
2022-06-01 09:57:28 +0430

    [ADD-FILE] all file was added into git

--- recommendation.py
+++ recommendation.py
@@ -68,3 +68,48 @@ class SequenceEncoder(object):
                           convert_to_tensor=True, device=self.device)
     return x.cpu()
 
+class GenresEncoder(object):
+
+  """The 'GenreEncoder' splits the raw column strings by 'sep' and converts
+  # individual elements to categorical labels."""
+
+  def __init__(self, sep='|'):
+    self.sep = sep
+
+  def __call__(self, df):
+    genres = set(g for col in df.values for g in col.split(self.sep))
+    mapping = {genre: i for i, genre in enumerate(genres)}
+
+    x = torch.zeros(len(df), len(mapping))
+    for i, col in enumerate(df.values):
+      for genre in col.split(self.sep):
+        x[i, mapping[genre]] = 1
+    return x
+
+class IdentityEncoder(object):
+  """The 'IdentityEncoder' takes the raw column values and converts them to
+  PyTorch tensors."""
+  def __init__(self, dtype=None, is_list=False):
+    self.dtype = dtype
+    self.is_list = is_list
+
+  def __call__(self, df):
+    if self.is_list:
+      return torch.stack([torch.tensor(el) for el in df.values])
+    return torch.from_numpy(df.values).to(self.dtype)
+
+movie_query = """
+MATCH (m:Movie)-[:IN_GENRE]->(genre:Genre)
+WITH m, collect(genre.name) AS genres_list
+RETURN m.movieId AS movieId, m.title AS title, apoc.text.join(genres_list, '|') AS genres, m.fastrp AS fastrp
+"""
+
+
+movie_x, movie_mapping = load_node(
+    movie_query,
+    index_col='movieId', encoders={
+        'title': SequenceEncoder(),
+        'genres': GenresEncoder(),
+        'fastrp': IdentityEncoder(is_list=True)
+    })
+
